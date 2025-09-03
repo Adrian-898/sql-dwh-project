fe8d60567147d98a5bbd016350376efcb723c48a
Adrian Torres <82057757+Adrian-898@users.noreply.github.com>
2025-09-03 17:37:49 -0400

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,20 @@
+/*
+Create Database & Schemas
+
+this script creates the database named dwh_project and sets up three schemas in it: 'bronze', 'silver', and 'gold'.
+
+INFO: this script will not run if a database with the same name already exists...
+*/
+
+USE master;
+
+CREATE DATABASE dwh_project;
+
+USE dwh_project;
+
+GO
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
